b5856da64661e58d657ec44fc234d2d1ea18a03d
Khush Patel <khush.6657@gmail.com>
2024-02-04 14:34:54 -0400

    frontend setup

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+
+function App() {
+  return (
+    <div className="text-red-500 flex justify-center">
+      <Button className="outline">Click Me</Button>
+    </div>
+  );
+}
+
+export default App;
