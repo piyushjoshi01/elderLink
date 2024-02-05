2138adb62afb6a023f1c19e53f54132b34e98d74
Khush Patel <kh472243@dal.ca>
2024-02-05 14:06:07 -0400

    edited frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <Button className='text-red-600'>
+        Click Me
+      </Button>
+    </>
+  )
+}
+
+export default App
