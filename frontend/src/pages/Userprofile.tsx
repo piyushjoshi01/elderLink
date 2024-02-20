c7c16224f30e4b07bef42b7f6393a31d2132f5ac
Riddho <riddhoriddho@gmail.com>
2024-02-20 11:04:14 -0400

    update-profile

--- frontend/src/pages/Userprofile.tsx
+++ frontend/src/pages/Userprofile.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
 import Navbar from "./Navbar";
 import Footer from "../components/ui/Footer";
 import UserModel from "@/models/UserModel";
@@ -33,6 +33,58 @@ const UserProfile = () => {
         console.error("Failed to fetch user details", error);
       });
   }, [localStorage.getItem("accessToken")]);
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [editedFirstName, setEditedFirstName] = useState<string>(user.firstName);
+  const [editedLastName, setEditedLastName] = useState<string>(user.lastName);
+  const [editedPhone, setEditedPhone] = useState<string>(user.phone);
+  const [editedAddress, setEditedAddress] = useState<{
+      street_name: string;
+      suite_number: string;
+      city: string;
+      state: string;
+      country: string;
+      pincode: string;
+  }>({
+      street_name: user.address.street_name,
+      suite_number: user.address.suite_number,
+      city: user.address.city,
+      state: user.address.state,
+      country: user.address.country,
+      pincode: user.address.pincode
+  });
+
+  const handleEdit = () => {
+      setIsEditing(!isEditing);
+  };
+
+  const handleSave = () => {
+      // Save changes
+      setIsEditing(false);
+      // You can perform further actions like API calls to update the user information
+  };
+
+  const handleChangeFirstName = (event: ChangeEvent<HTMLInputElement>) => {
+      setEditedFirstName(event.target.value);
+  };
+
+  const handleChangeLastName = (event: ChangeEvent<HTMLInputElement>) => {
+      setEditedLastName(event.target.value);
+  };
+
+  const handleChangePhone = (event: ChangeEvent<HTMLInputElement>) => {
+      setEditedPhone(event.target.value);
+  };
+
+  const handleChangeAddress = (event: ChangeEvent<HTMLInputElement>, key: string) => {
+      setEditedAddress({
+          ...editedAddress,
+          [key]: event.target.value
+      });
+  };
+
+  const getInitials = (firstName: string, lastName: string) => {
+      return `${firstName[0]}${lastName[0]}`;
+  };
 
   return (
     <>
@@ -43,12 +95,15 @@ const UserProfile = () => {
             <h1 className="text-3xl font-bold tracking-wider text-lime-800 mb-4">
               User Profile{" "}
             </h1>
+            <div onClick={handleEdit} className="cursor-pointer text-blue-500 mb-2">
+                            {isEditing ? "Cancel" : "Edit"}
+                        </div>
 
             <div className="flex items-center justify-between mb-6">
               <div className="flex-shrink-0">
-                <div className="w-48 h-48 flex items-center justify-center bg-gray-300 text-gray-600 text-4xl font-bold rounded-full">
-                  {(user.firstName, user.lastName)}
-                </div>
+              <div className="w-48 h-48 flex items-center justify-center bg-gray-300 text-gray-600 text-4xl font-bold rounded-full">
+                                    {getInitials(editedFirstName, editedLastName)}
+                                </div>
               </div>
               <div className="ml-6 text-right">
                 <div className="rounded-md bg-gray-200 p-2 mb-2 font-medium">
@@ -65,20 +120,41 @@ const UserProfile = () => {
             </div>
             <div>
               <p className="text-lime-800 mb-4">
-                <strong>First Name:</strong> {user.firstName}
+                <strong>First Name:</strong> {isEditing ? <input type="text" value={editedFirstName} onChange={handleChangeFirstName} /> : editedFirstName}{user.firstName}
               </p>
               <p className="text-lime-800 mb-4">
-                <strong>Last Name:</strong> {user.lastName}
+                <strong>Last Name:</strong> {isEditing ? <input type="text" value={editedLastName} onChange={handleChangeLastName} /> : editedLastName}{user.lastName}
               </p>
               <p className="text-lime-800 mb-4">
                 <strong>Email:</strong> {user.email}
               </p>
               <p className="text-lime-800 mb-4">
-                <strong>Phone:</strong> {user.phone}
+                <strong>Phone:</strong> {isEditing ? <input type="text" value={editedPhone} onChange={handleChangePhone}/>:editedPhone} {user.phone}
               </p>
               <p className="text-lime-800 mb-4">
                 <strong>Birth Date:</strong> {user.birthDate}
               </p>
+              {isEditing && (
+                                <div>
+                                    <p className="text-lime-800 mb-4"><strong>Password:</strong> <input type="password" value={user.password} onChange={() => {}} /></p>
+                                    <p className="text-lime-800 mb-4"><strong>Address:</strong></p>
+                                    <p className="text-lime-800 mb-4">
+                                        <input type="text" value={editedAddress.street_name} onChange={(e) => handleChangeAddress(e, 'street_name')} />, 
+                                        <input type="text" value={editedAddress.suite_number} onChange={(e) => handleChangeAddress(e, 'suite_number')} />, 
+                                        <input type="text" value={editedAddress.city} onChange={(e) => handleChangeAddress(e, 'city')} />, 
+                                        <input type="text" value={editedAddress.state} onChange={(e) => handleChangeAddress(e, 'state')} />, 
+                                        <input type="text" value={editedAddress.country} onChange={(e) => handleChangeAddress(e, 'country')} />, 
+                                        <input type="text" value={editedAddress.pincode} onChange={(e) => handleChangeAddress(e, 'pincode')} />
+                                    </p>
+                                </div>
+                            )}
+                            <div className="flex items-center mb-2">
+                                <strong className="text-lime-800 mr-2">Address:</strong>
+                                <span>{`${user.address.street_name}, ${user.address.suite_number}, ${user.address.city}, ${user.address.state}, ${user.address.country}, ${user.address.pincode}`}</span>
+                            </div>
+                            {isEditing && (
+                                <button onClick={handleSave} className="px-4 py-2 bg-lime-800 text-white rounded-md">Save</button>
+                            )}
               {/* <div className="flex items-center mb-2">
                 <strong className="text-lime-800 mr-2">Address:</strong>
                 <span>{`${user.address.street_name}, ${user.address.suite_number}, ${user.address.city}, ${user.address.state}, ${user.address.country}, ${user.address.pincode}`}</span>
