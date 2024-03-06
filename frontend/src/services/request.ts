73f000925ad469b7f4921cf3f74e7433a459c514
Piyush Rakeshbhai Joshi <py287300@dal.ca>
2024-03-06 10:19:14 -0400

    Running Complete Webiste

--- frontend/src/services/request.ts
+++ frontend/src/services/request.ts
@@ -2,10 +2,8 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const request = axios.create({
-  // baseURL: "https://web1.anasource.com/BookStore/api/BookStore/", // url = base url + request url
-  baseURL: "http://localhost:8080/",
-  // baseURL: "https://helperland1.azurewebsites.net/",
-  //  baseURL: "http://192.168.1.20/",
+  baseURL: "http://csci5308vm12.research.cs.dal.ca:8080/",
+
   timeout: 12400000,
   responseType: "json",
 });
