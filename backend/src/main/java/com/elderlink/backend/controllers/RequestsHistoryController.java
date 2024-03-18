b72f534f93e05d70ce24337488316b2b77f730b1
Riddho <riddhoriddho@gmail.com>
2024-03-18 17:23:35 -0300

    getRequestHistory API Implementation

--- backend/src/main/java/com/elderlink/backend/controllers/RequestsHistoryController.java
+++ backend/src/main/java/com/elderlink/backend/controllers/RequestsHistoryController.java
@@ -1,13 +1,17 @@
 package com.elderlink.backend.controllers;
 
 import com.elderlink.backend.domains.dto.RequestHistoryDto;
+import com.elderlink.backend.domains.entities.RequestHistoryEntity;
 import com.elderlink.backend.mappers.impl.RequestHistoryMapper;
 import com.elderlink.backend.services.RequestsHistoryService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/requestsHistory")
@@ -19,5 +23,28 @@ public class RequestsHistoryController{
     @Autowired
     private RequestHistoryMapper requestHistoryMapper;
 
+    @GetMapping("/requestsByElderPersonId/{elderPersonId}")
+    public ResponseEntity<List<RequestHistoryDto>> getRequestsHistoryByElderPersonId(
+            @Valid @PathVariable("elderPersonId") Long elderPersonId
+    ){
+        List<RequestHistoryEntity> requestHistoryEntities = requestHistoryService.getRequestHistoriesByElderPersonId (elderPersonId);
+        return ResponseEntity.status (HttpStatus.OK).body (
+                requestHistoryEntities.stream ()
+                        .map (requestHistoryMapper::toDto)
+                        .collect(Collectors.toList())
+        );
+    }
+
+    @GetMapping("/{requestId}")
+    public ResponseEntity<List<RequestHistoryDto>> getRequestHistoryByRequestId(
+            @Valid @PathVariable("requestId") Long requestId
+    ){
+        List<RequestHistoryEntity> requestHistoryEntities = requestHistoryService.getRequestHistoriesByRequestId(requestId);
+        return ResponseEntity.status (HttpStatus.OK).body (
+                requestHistoryEntities.stream ()
+                        .map (requestHistoryMapper::toDto)
+                        .collect(Collectors.toList())
+        );
+    }
 
 }
--- backend/src/main/java/com/elderlink/backend/services/RequestsHistoryService.java
+++ backend/src/main/java/com/elderlink/backend/services/RequestsHistoryService.java
@@ -2,8 +2,13 @@ package com.elderlink.backend.services;
 
 import com.elderlink.backend.domains.entities.RequestHistoryEntity;
 
+import java.util.List;
+
 public interface RequestsHistoryService{
 
     public RequestHistoryEntity createRequestHistory(RequestHistoryEntity requestHistoryEntity);
 
+    public List<RequestHistoryEntity> getRequestHistoriesByRequestId(Long requestId);
+
+    public List<RequestHistoryEntity> getRequestHistoriesByElderPersonId(Long elderPersonId);
 }
