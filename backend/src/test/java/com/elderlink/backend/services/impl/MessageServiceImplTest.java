bcdbefc79b2c7a7b77b9aa05302dce3e19bc524c
Khush Patel <kh472243@dal.ca>
2024-03-20 18:21:40 -0300

    Test cases for updateBlog

--- backend/src/test/java/com/elderlink/backend/services/impl/MessageServiceImplTest.java
+++ backend/src/test/java/com/elderlink/backend/services/impl/MessageServiceImplTest.java
@@ -12,6 +12,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
