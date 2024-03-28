9fbc7a0898ce4b5a30ee0c278661ca2294bf482a
Abhishek Kapoor <ab210637@dal.ca>
2024-03-27 23:14:21 -0300

    Fix Frontend

--- frontend/src/pages/BlogCard.tsx
+++ frontend/src/pages/BlogCard.tsx
@@ -1,5 +1,5 @@
 import { useUser } from "@/context/UserContext";
-import { useEffect,useState } from "react";
+// import { useEffect,useState } from "react";
 import { toast } from "react-toastify";
 
 const BlogCard = ({ blog,onDelete }: any) => {
